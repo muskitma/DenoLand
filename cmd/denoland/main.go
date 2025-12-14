a7de1d38ed88c6cc0b36c7770e8fccc526254f67
Muski <143282825+muskitma@users.noreply.github.com>
2025-12-14 22:35:47 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/denoland/main.go
@@ -0,0 +1,23 @@
+// cmd/denoland/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "denoland/internal/denoland"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := denoland.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
